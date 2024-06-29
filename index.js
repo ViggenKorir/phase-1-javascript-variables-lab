435013f29085d742599f51db8a13325387703385
Viggen <koriremanuel0@gmail.com>
2024-06-29 19:37:31 +0300

    Variable declaration

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Code your solution in this file!
+const companyName = 'Scuber';
+let mostProfitableNeighborhood = 'Chelsea';
+let companyCeo = 'Susan Smith';
+let companyLocation = 'San Francisco, California';
+let companyEmployees = 100000;
